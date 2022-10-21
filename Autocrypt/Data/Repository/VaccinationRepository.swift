455010f005c38fb4e8187569b79b90afa62b869e
Siwon Kim <siyeon0616@naver.com>
2022-10-21 21:51:47 +0900

    feat: VaccinationRepository 생성 #3
    - NetworkProvider가 채택한 프로토콜인 NetworkProvidable에 Observable을 리턴하는 request 메서드 추가

---
+++ Autocrypt/Data/Repository/VaccinationRepository.swift
@@ -0,0 +1,24 @@
+//
+//  VaccinationRepository.swift
+//  Autocrypt
+//
+//  Created by Siwon Kim on 2022/10/21.
+//
+
+import RxSwift
+
+protocol VaccinationRepositoryType {
+    func fetchVaccinationList(page: String) -> Observable<VaccinationCenterList>
+}
+
+final class VaccinationRepository: VaccinationRepositoryType {
+    private let networkProvider: NetworkProvidable
+    
+    init(networkProvider: NetworkProvidable) {
+        self.networkProvider = networkProvider
+    }
+    
+    func fetchVaccinationList(page: String) -> Observable<VaccinationCenterList> {
+        return networkProvider.request(with: EndpointGenerator.fetchVaccinationList(page: page))
+    }
+}
