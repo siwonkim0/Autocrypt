179249d4eefdb53a992c5c46b09095efcd27baf2
Siwon Kim <siyeon0616@naver.com>
2022-10-22 22:38:11 +0900

    chore: 코드 위치 변경

--- Autocrypt/Presentation/Coordinator/VaccinationListCoordinator.swift
+++ Autocrypt/Presentation/Coordinator/VaccinationListCoordinator.swift
@@ -21,22 +21,12 @@ final class VacinationListCoordinator: Coordinator, VaccinationListViewControlle
     }
     
     func start() {
-        let viewController = setViewController()
-        setNavigationController(with: viewController)
-    }
-    
-    private func setViewController() -> UIViewController {
         let networkProvider = NetworkProvider()
         let repository = VaccinationRepository(networkProvider: networkProvider)
         let viewModel = VaccinationListViewModel(repository: repository)
         let listViewController = VaccinationListViewController(viewModel: viewModel)
         listViewController.coordinator = self
-        return listViewController
-    }
-    
-    private func setNavigationController(with viewController: UIViewController) {
-        navigationController.navigationBar.topItem?.title = "예방접종센터 리스트"
-        navigationController.pushViewController(viewController, animated: true)
+        navigationController.setViewControllers([listViewController], animated: true)
     }
     
     func showDetailViewController(at viewController: UIViewController, of model: VaccinationCenter) {
