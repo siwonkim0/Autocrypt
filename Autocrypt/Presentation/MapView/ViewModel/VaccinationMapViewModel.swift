b5a67287e4ca6a09b493166a7189fbeca138b069
Siwon Kim <siyeon0616@naver.com>
2022-10-23 14:12:23 +0900

    feat: VaccinationMapViewModel 생성 #9

--- Autocrypt/Presentation/MapView/ViewModel/VaccinationMapViewModel.swift
+++ Autocrypt/Presentation/MapView/ViewModel/VaccinationMapViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+final class VaccinationMapViewModel {
+    let model: VaccinationCenter
+    
+    init(model: VaccinationCenter) {
+        self.model = model
+    }
+    
+}
+
+struct VaccinationCenterLocation {
+    let latitute: Double
+    let longitude: Double
+}
